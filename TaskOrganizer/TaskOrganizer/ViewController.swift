c47607625abf4efcd8f999f5c2aedf95725a8fdc
alvaro-minzala <71344754+alvaro-minzala@users.noreply.github.com>
2021-07-14 09:45:51 +0200

    Added actual code into repository
    lol

---
+++ TaskOrganizer/TaskOrganizer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TaskOrganizer
+//
+//  Created by Alvaro Minzala on 13/7/21.
+//  Copyright © 2021 alvaro-minzala. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ TaskOrganizer/TaskOrganizerTests/TaskOrganizerTests.swift
@@ -0,0 +1,36 @@
+//
+//  TaskOrganizerTests.swift
+//  TaskOrganizerTests
+//
+//  Created by Alvaro Minzala on 13/7/21.
+//  Copyright © 2021 alvaro-minzala. All rights reserved.
+//
+
+import XCTest
+@testable import TaskOrganizer
+
+class TaskOrganizerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
